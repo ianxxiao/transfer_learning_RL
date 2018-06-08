3dc5f593ea7a3513e8b9d8e78344dba6c4b38ac5
Mr Mrmrsxiao Mrs Xiao <mrmrsxiao@Mrs-iMac.local>
2018-06-08 09:44:31 -0400

    add architecture screenshot

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ from trainer import trainer
 if __name__ == "__main__":
     
     action_space = [-20, -10, -3, -1, 0, 1, 3, 10, 20]
-    trainer = trainer(10, action_space, episode = 1000)
+    trainer = trainer(10, action_space, episode = 20000)
     trainer.start()
     
     del trainer
